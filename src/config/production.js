2263761d53ac78acea3eec14afd8168ec032907f
Andres Coto <andres.coto3539@gmail.com>
2019-02-10 19:32:13 -0600

    url global, y limpieza de campos en login

---
+++ src/config/production.js
@@ -0,0 +1,25 @@
+module.exports = {
+    app: {
+      name: 'Linafa Manager',
+      url: 'http://192.168.0.34:3000',
+    }
+  };
+
+  /* module.exports = {
+    app: {
+      name: 'Linafa Manager',
+    },
+    mail: {
+      auth: {
+        api_key: 'key-f108e891c806c3ca112e59a047252fbd',
+        domain: 'sandbox063a58d8bead4a048ef050c595375cda.mailgun.org',
+      },
+      mailSender: 'Redisrupt Invites <postmaster@sandbox063a58d8bead4a048ef050c595375cda.mailgun.org>',
+    },
+    auth: {
+      maxLoginAttempts: 10,
+    },
+    tokens: {
+      validPeriodInDays: 20,
+    },
+  };*/
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import config from '../../config/production.js';
 
 @Component({
   selector: 'page-home',
@@ -10,73 +11,68 @@ import { Observable } from 'rxjs/Observable';
 
 export class HomePage {
 
-result: any;
-region_name: any;
-region='';
-public data: Observable<any>;
-public name: any;
-public password: any;
+  result: any;
+  region_name: any;
+  region = '';
+  public data: Observable<any>;
+  public name: any;
+  public password: any;
 
 
-constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient) {
-
-
-}
-
-
-
-async adminMenu() {
-  if (this.name == null || this.password == null) {
-
-    alert("Porfavor ingrese todos los datos correspondientes");
-
-
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient) {
+    console.log(config.app.url);
   }
 
-  else {
-
-    var url = 'http://localhost:3000/login';
-    let postData = {
-      "user_name": this.name,
-      "pass_login": this.password
-    };
-
-  
-
-    this.data = this.http.post(url, postData);
-    this.data.subscribe(data => {
 
 
-      console.log(data[0].result+"result");
-      console.log(data[0].name +"todo");
+  async adminMenu() {
 
-      
-        if(data[0].result=="incorrect" || data[0].result=="not exist"){
+    if (this.name == null || this.password == null) {
+
+      alert("Porfavor ingrese todos los datos correspondientes");
+
+
+    }
+
+    else {
+      var url = `${config.app.url}/login`;
+      console.log(url);
+      let postData = {
+        "user_name": this.name,
+        "pass_login": this.password
+      };
+
+
+
+      this.data = this.http.post(url, postData);
+      this.data.subscribe(data => {
+
+
+        console.log(data[0].result + "result");
+        console.log(data[0].name + "todo");
+        console.log(data[0]);
+
+        if (data[0].result == "incorrect" || data[0].result == "not exist" || data[0].programaStatus == "0") {
           alert("usuario o contraseña incorrecta intente de nuevo");
-        }   
-      
-      if(data[0].role=="god"){
-        this.navCtrl.push('AdminMenuPage');
-      }
-      if(data[0].role=="admin"){
-        this.navCtrl.push('AdminMenuPage');
-      }
-      if(data[0].role=="manager"){
-        console.log(data[0].name);
-        this.region = data[0].name;
-        this.navCtrl.push('ManagersMenuPage',{region:this.region});
-      }
+          this.name = null;
+          this.password = null;
+        }
 
-      if(data[0].result=="incorrect"||data[0].result=="not exist"){
-        alert("usuario o contraseña incorrecta");
-      }
-      
+        if (data[0].role == "god" && data[0].programaStatus == "1") {
+          this.navCtrl.push('AdminMenuPage');
+        }
+        if (data[0].role == "admin" && data[0].programaStatus == "1") {
+          this.navCtrl.push('AdminMenuPage');
+        }
+        if (data[0].role == "manager" && data[0].programaStatus == "1") {
+          console.log(data[0].name);
+          this.region = data[0].name;
+          this.navCtrl.push('ManagersMenuPage', { region: this.region });
+        }
+      });
 
-
-    });
-
+    }
   }
-}
 
 }
 
